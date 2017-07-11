ed82edf54fb17e5f0dccb5b6b35c50294333ee9a
TreeNewBeee <9519380@qq.com>
2017-07-11 23:55:02 +0800

    Part Of League Management

---
+++ league/classTable.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TreeNewBeee
+ * Date: 2017-07-10
+ * Time: 16:22
+ */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<?php session_start(); ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <title></title>
+</head>
+
+<body>
+<?php
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+} else {
+    echo "<script>alert('先登陆。。。!');location.href='index.php';</script>";
+}
+
+$branch = isset($_GET['branch']) ? $_GET['branch'] : NULL;    // 获取单位
+
+?>
+<div class="container">
+
+
+    <div class="row">
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-1">
+                <h3>团课开展情况-<?php echo $branch ?></h3>
+                <hr>
+            </div>
+
+        </div>
+
+        <?php
+        require_once '../db_login.php';
+        $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+        if ($conn->connect_error) die($conn->connect_error);
+        mysqli_set_charset($conn, 'utf8');
+
+        $query = "select * from `person` WHERE `name` = '" . $_SESSION['username'] . "'";
+        $result = $conn->query($query);
+        $row = $result->fetch_array();
+        if ($row['branch'] == $branch) {
+            echo <<<PRINT
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-1">
+                        <a class="btn btn-default" href="classCreate.php?branch={$branch}" role="button">新增</a>
+                        <a class="btn btn-default" href="classDelete.php?branch={$branch}" role="button">删除</a>
+                        
+                    </div>
+        
+                </div>
+
+PRINT;
+
+        }
+
+        ?>
+
+        <br>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <table class="table table-condensed">
+                    <tr>
+                        <th width="10%" class="text-center">序号</th>
+                        <th width="15%" class="text-center">年份</th>
+                        <th width="15%" class="text-center">季度</th>
+                        <th class="text-center">下载</th>
+                    </tr>
+                    <?php
+                    require_once '../db_login.php';
+                    $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+                    if ($conn->connect_error) die($conn->connect_error);
+                    mysqli_set_charset($conn, 'utf8');
+
+                    $query = "SELECT * FROM `plans` WHERE `branch` = '" . $branch . "' AND `type` = '团课'  ORDER BY `plans`.`year` DESC";
+                    $result = $conn->query($query);
+                    // TODO: 未完成根据时间限制查看
+                    $index = 1;
+                    while ($row = $result->fetch_array()) {
+                        echo <<<PRINTTABLE
+                            <tr>
+                                <td class="text-center">{$index}</td>
+                                <td class="text-center">{$row['year']}</td>
+                                <td class="text-center">{$row['season']}</td>
+                                <td class="text-center"><a class="btn btn-default btn-sm" href="../Files/{$row['fileName']}" role="button">查看</a> </td>
+                            </tr>
+
+PRINTTABLE;
+                        $index++;
+
+                    }
+
+                    ?>
+
+
+                </table>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
+
+</body>
+</html>
+
---
+++ league/leaderDelete.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TreeNewBeee
+ * Date: 2017-07-11
+ * Time: 22:10
+ */
+?>
+
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+    <?php session_start(); ?>
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <title></title>
+    </head>
+
+    <body>
+    <?php
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+    } else {
+        echo "<script>alert('先登陆。。。!');location.href='index.php';</script>";
+    }
+
+    $branch = isset($_GET['branch']) ? $_GET['branch'] : NULL;    // 获取单位
+
+    ?>
+
+    <div class="container">
+
+
+        <div class="row">
+
+            <div class="row">
+                <div class="col-md-8 col-md-offset-1">
+                    <h2>删除支委会开展情况-<?php echo $branch?></h2>
+                    <hr/>
+                </div>
+
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-10 col-md-offset-1">
+                    <form method="post" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="year">年份</label>
+                                    <input type="text" class="form-control" id="year" name="year" placeholder="请填写四位年份如：<?php echo date("Y") ?>">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="month">月份</label>
+                                    <div class="form-group">
+                                        <select class="form-control" id="month" name="month">
+                                            <option value="1">一月</option>
+                                            <option value="2">二月</option>
+                                            <option value="3">三月</option>
+                                            <option value="4">四月</option>
+                                            <option value="5">五月</option>
+                                            <option value="6">六月</option>
+                                            <option value="7">七月</option>
+                                            <option value="8">八月</option>
+                                            <option value="9">九月</option>
+                                            <option value="10">十月</option>
+                                            <option value="11">十一月</option>
+                                            <option value="12">十二月</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-default">删除</button>
+                    </form>
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+    </body>
+    </html>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TreeNewBeee
+ * Date: 2017-04-27
+ * Time: 01:11
+ */
+// TODO：未添加权限
+
+if (isset($_POST['year'])){
+    require_once '../db_login.php';
+    $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+    if ($conn->connect_error) die($conn->connect_error);
+    mysqli_set_charset($conn, 'utf8');
+
+    $query = "SELECT * FROM `plans` WHERE `branch` = '".$branch."' AND `year` = '".$_POST['year']."' AND `month` = '".$_POST['month']."' AND `type` = '支委会'";
+    $result = $conn->query($query);
+    $row = $result->fetch_array();
+    $filename = $row['fileName'];
+
+    // 删除文件
+    if (empty($filename)) {
+        $file = $filename;
+        if (!unlink($file))
+        {
+            echo ("Error deleting $file");
+        }
+        else
+        {
+            echo ("Deleted $file");
+        }
+    }
+
+    // 更新数据库
+//    echo $_POST['year'];
+//    echo $_POST['month'];
+    $query = "DELETE FROM `plans` WHERE `branch` = '".$branch."' AND `year` = '".$_POST['year']."' AND `month` = '".$_POST['month']."' AND `type` = '支委会'";
+    $conn->query($query);
+    $conn->close();
+
+    echo <<<JUMP
+            <script language="JavaScript">
+                   alert("删除成功");
+                   self.location='leaderTable.php?branch={$branch}';
+            </script>
+JUMP;
+
+}
+
